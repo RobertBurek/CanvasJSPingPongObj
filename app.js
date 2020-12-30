9147e1c6c95242c29438ce88259e2a296143542a
Robert Burek <robertburek@wp.pl>
2020-12-31 00:22:39 +0100

    app.js cd..

--- app.js
+++ app.js
@@ -5,5 +5,20 @@ const ctx = cnv.getContext('2d');
 const cnvW = cnv.width = 1200;
 const cnvH = cnv.height = 600;
 
-console.log(cnvW);
-console.log(cnvH);
+
+const ballSize = 16;
+
+
+function Ball(ballSize, colorBall, positionXBall, positionYBall){
+    this.color = colorBall;
+    this.positionX = positionXBall;
+    this.positionY = positionYBall;
+    this.radius = ballSize / 2;
+    this.speedX = 4;
+    this.speedY = 4;
+    this.directionX = true;
+    this.directionY = true;
+    this.center = ballSize / 2;
+}
+
+
