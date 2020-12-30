217c240d0093e75598baf07123e9779787b4cbaa
Robert Burek <robertburek@wp.pl>
2020-12-30 23:43:23 +0100

    cd..

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const cnv = document.querySelector('canvas');
-
 const ctx = cnv.getContext('2d');
 
 const cnvW = cnv.width = 1200;
 const cnvH = cnv.height = 600;
+
+const ballSize = 16;
+
+
+function ball(){
+    ctx.arc(cnvW/2 - ballSize/2,cnvH/2 - ballSize/2, ballSize/2, 0, 2*Math.PI);
+    ctx.fillStyle = 'lime';
+    ctx.fill();
+}
 
 function court() {
     ctx.fillStyle = 'black';
@@ -11,3 +19,4 @@ function court() {
 }
 
 court();
+ball();
