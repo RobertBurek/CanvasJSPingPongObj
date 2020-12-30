820fa32cf63af9c181ec482b9979a99436affc87
Robert Burek <robertburek@wp.pl>
2020-12-30 21:25:07 +0100

    cd..

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ const ctx = cnv.getContext('2d');
 const cnvW = cnv.width = 1200;
 const cnvH = cnv.height = 600;
 
-console.log(cnvW);
-console.log(cnvH);
+function court() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,cnvW,cnvH);
+}
+
+court();
