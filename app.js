a1dc0ae9053d270fa0d2ee78d4e7b4548362a8bc
Robert Burek <robertburek@wp.pl>
2021-01-03 19:23:26 +0100

    Kolizja z krawędziami canvas

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const ctx = cnv.getContext('2d');
 
 const cnvW = cnv.width = 1200;
 const cnvH = cnv.height = 600;
-const ballSize = 16;
+const radiusBall = 8;
 const widthRocket = 20;
 const heightRocket = 150;
 const deltaRocket = 30;
 
 class Ball {
-    constructor (ballSize, colorBall, positionXBall, positionYBall){
-        this.color = colorBall;
-        this.positionX = positionXBall;
-        this.positionY = positionYBall;
-        this.radius = ballSize / 2;
+    constructor (radiusB, colorB, positionXB, positionYB, directionXB, directionYB){
+        this.color = colorB;
+        this.positionX = positionXB;
+        this.positionY = positionYB;
+        this.radius = radiusB;
         this.speedX = 4;
         this.speedY = 4;
-        this.directionX = true;
-        this.directionY = true;
-        this.center = ballSize / 2;
+        this.directionX = directionXB;
+        this.directionY = directionYB;
+        this.center = radiusB;
         }
 
     draw() {
@@ -28,8 +28,16 @@ class Ball {
         ctx.arc(this.positionX,this.positionY, this.radius, 0, 2*Math.PI);
         ctx.fill(); 
         ctx.closePath();
-        this.positionX += 2;
-        this.positionY += -2;
+        this.positionX += this.speedX;
+        this.positionY += this.speedY;
+      }
+
+      collisionX() {
+        this.speedX *= -1;
+
+      }
+      collisionY() {
+        this.speedY *= -1;
       }
 }
 
@@ -57,18 +65,22 @@ function court() {
     ctx.fillRect(0,0,cnvW,cnvH);
 }
 
-const ball1 = new Ball(ballSize,'lime',250,450);
+const ball1 = new Ball(radiusBall, 'lime', 250, 450, true, false);
+const ball2 = new Ball(radiusBall, 'yellow', 50, 150, false, true);
 const playerRocket = new Rocket(widthRocket, heightRocket, 'blue', deltaRocket, 250);
 const computerRocket = new Rocket(widthRocket, heightRocket, 'red', (cnvW - deltaRocket - widthRocket), 150);
 
 const gameElements = [];
 
-gameElements.push(ball1,playerRocket,computerRocket);
+gameElements.push(ball1, playerRocket, computerRocket, ball2);
 
 function start(gameElements){
     court();
     gameElements.forEach(element => {
         element.draw();
+        if (element.positionY <= radiusBall || element.positionY >= cnv.height - radiusBall) element.collisionY();
+        if (element.positionX <= radiusBall || element.positionX >= cnv.width - radiusBall) element.collisionX();
+
     });
 }
 
