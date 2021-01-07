e9cd999d1324c67da7c6d888f5c41079c8622978
Robert Burek <robertburek@wp.pl>
2021-01-07 21:54:16 +0100

    Kolizja cd.. - lisContact

--- app.js
+++ app.js
@@ -8,25 +8,21 @@ const radiusBall = 8;
 const widthRocket = 20;
 const heightRocket = 150;
 const deltaRocket = 30;
-const interval = 15;
+const interval = 2000;
 let collisionElements = [];
 
 class CollisionElement{
-    // constructor (Element1, directionEl1, deltaXEl1, deltaYEl1, Element2, directionEl2, deltaXEl2, deltaYEl2) {
     constructor (Element1, Element2, directionEl1, directionEl2, deltaX, deltaY) {
         this.Element1 = Element1;
         this.Element2 = Element2;
         this.directionEl1 = directionEl1;
         this.directionEl2 = directionEl2;
         this.deltaX = deltaX;
-        // this.deltaXEl2 = deltaXEl2;
         this.deltaY = deltaY;
-        // this.deltaYEl2 = deltaYEl2;
     }
 }
 
 class Ball {
-    // inCollision = false;
     constructor (radiusB, colorB, positionXB, positionYB, speedXB, speedYB){
         this.color = colorB;
         this.positionX = positionXB;
@@ -47,10 +43,9 @@ class Ball {
         ctx.closePath();
         // this.positionX += this.speedX;
         // this.positionY += this.speedY;
-        this.inCollision = false;
       }
 
-      direction(){
+    direction(){
         if (this.speedX > 0 && this.speedY > 0 ) return "leftTop";
         if (this.speedX < 0 && this.speedY > 0 ) return "rightTop";
         if (this.speedX > 0 && this.speedY < 0 ) return "leftBottom";
@@ -59,28 +54,33 @@ class Ball {
         if (this.speedX < 0 && this.speedY == 0 ) return "right";
         if (this.speedX == 0 && this.speedY > 0 ) return "top";
         if (this.speedX == 0 && this.speedY < 0 ) return "bottom";
-      }
+    }
 
-      isContact(){
+    isContact(){
         for (let i = 0; i <= gameElements.length - 1; i++) {
             let e = gameElements[i];
             if (this === e) continue;
             let deltaX = this.positionX - e.positionX;
             let deltaY = this.positionY - e.positionY;
-            // console.log(e);
-        let dystans = Math.pow(deltaX, 2) + Math.pow(deltaY, 2);
-        // console.log(dystans);
-        if (dystans <= Math.pow(2 * this.radius, 2)) {
-            // e.inCollision = true;
-            if ((collisionElements.length == 0) || (!this.inCollision))
-            {
-                console.log(dystans);
-                collisionElements.push(new CollisionElement(this, e, this.direction(), e.direction(), deltaX, deltaY));
-                console.log(collisionElements);
-                // this.inCollision = true;
-            } 
+            let dystans = Math.pow(deltaX, 2) + Math.pow(deltaY, 2);
+            if (dystans <= Math.pow(2 * this.radius, 2)) {
+                this.positionX -= this.speedX;
+                this.positionY -= this.speedY;
+                e.positionX -= e.speedX;
+                e.positionY -= e.speedY;
+                if (collisionElements.length == 0) collisionElements.push(new CollisionElement(this, e, this.direction(), e.direction(), deltaX, deltaY));
+                else {
+                    for(let i = 0; i <= collisionElements.length-1; i++){
+                        if (((this===collisionElements[i].Element2)&&(collisionElements[i].Element1===e))||((e===collisionElements[i].Element2)&&(collisionElements[i].Element1===this))){
+                            break;
+                        } else {
+                            collisionElements.push(new CollisionElement(this, e, this.direction(), e.direction(), deltaX, deltaY));
+                            break;
+                        }
+                    }
+                };
+            }
         }
-      }
     }
 
     collision(){
@@ -137,19 +137,19 @@ function court() {
     ctx.fillRect(0,0,cnvW,cnvH);
 }
 
-const ball1 = new Ball(radiusBall, 'white', 50, 250, 4, 4);
+const ball1 = new Ball(radiusBall, 'white', 50, 250, 6, 6);
 const ball2 = new Ball(radiusBall, 'yellow', 55, 350, 4, -4);
-const ball3 = new Ball(radiusBall, 'red', 100, 250, -4, 4);
+const ball3 = new Ball(radiusBall, 'red', 100, 250, -6, 6);
 const ball4 = new Ball(radiusBall, 'blue', 50, 300, 0, 4);
 const ball5 = new Ball(radiusBall, 'green', 470, 257, 5, -5);
 const ball6 = new Ball(radiusBall, 'red', 550, 350, 4, -6);
 const ball7 = new Ball(radiusBall, 'blue', 655, 450, 5, 6);
 const ball8 = new Ball(radiusBall, 'green', 756, 250, 4, -3);
 const ball9 = new Ball(radiusBall, 'lime', 780, 254, 4, 4);
-const ball10 = new Ball(radiusBall, 'yellow', 880, 387, 3, -5);
-const ball11 = new Ball(radiusBall, 'red', 990, 25, 5, 5);
-const ball12 = new Ball(radiusBall, 'blue', 1008, 560, -6, -3);
-const ball13 = new Ball(radiusBall, 'lime', 820, 333, 4, );
+const ball10 = new Ball(radiusBall, 'yellow', 220, 387, 3, -5);
+const ball11 = new Ball(radiusBall, 'red', 790, 25, 5, 5);
+const ball12 = new Ball(radiusBall, 'blue', 458, 360, -6, -3);
+const ball13 = new Ball(radiusBall, 'lime', 620, 333, 4, 5);
 const ball14 = new Ball(radiusBall, 'yellow', 680, 222, -4, 3);
 const ball15 = new Ball(radiusBall, 'lime', 355, 111, 5, 4);
 const ball16 = new Ball(radiusBall, 'green', 156, 68, -5, 5);
@@ -159,26 +159,43 @@ const computerRocket = new Rocket(widthRocket, heightRocket, 'red', (cnvW - delt
 const gameElements = [];
 
 // gameElements.push(ball1, playerRocket, computerRocket, ball2, ball3, ball4, ball5);
-gameElements.push(ball1, ball2, ball3, ball4, ball5, ball6, ball7,  ball8, ball9, ball10, ball11, ball12, ball13, ball14, ball15, ball16);
+// gameElements.push(ball1, ball2, ball3, ball4, ball5, ball6, ball7,  ball8, ball9, ball10, ball11, ball12, ball13, ball14, ball15, ball16);
 // gameElements.push(ball1, ball4, ball8,ball10, ball13);
 // gameElements.push(ball1, ball2);
-// gameElements.push(ball1, ball3);
+gameElements.push(ball1, ball3);
 
+let k = 0;
 
 function start(gameElements){
+    // console.log(++k);
     court();
     gameElements.forEach(element =>{
         element.draw();
     });
     gameElements.forEach(element =>{
-        if (element.positionY <= radiusBall || element.positionY >= cnv.height - radiusBall) element.collisionY();
-        if (element.positionX <= radiusBall || element.positionX >= cnv.width - radiusBall) element.collisionX();
+        // if (element.positionY <= radiusBall || element.positionY >= cnv.height - radiusBall) element.collisionY();
+        // if (element.positionX <= radiusBall || element.positionX >= cnv.width - radiusBall) element.collisionX();
         element.isContact();
     });
-    gameElements.forEach(element =>{
-        element.collision();
-    });
+    if (collisionElements.length>0) console.log(collisionElements);
+    // collisionList = [];
+    // collisionElements.forEach(element =>{
+    //     for(let i = 0; i <= collisionElements.length-1; i++){
+    //         // console.log(element.Element1.color + " - " + collisionElements[i].Element2.color)
+    //         // console.log(element.Element1===collisionElements[i].Element2);
+    //         //  + ' - ' + collisionElements[i].Element1!==element.Element2);
+    //         console.log(((element.Element1===collisionElements[i].Element2)&&(collisionElements[i].Element1===element.Element2)));
+    //         if ((element.Element1===collisionElements[i].Element2)&&(collisionElements[i].Element1===element.Element2)){
+    //             delete collisionElements[i];
+    //         } else collisionList.push(element);
+    //     }
+    //     // console.log(collisionList);
+    // });
     collisionElements = [];
+    gameElements.forEach(element =>{
+        if (element.positionY <= radiusBall || element.positionY >= cnv.height - radiusBall) element.collisionY();
+        if (element.positionX <= radiusBall || element.positionX >= cnv.width - radiusBall) element.collisionX();
+    });
 }
 
 function game(){
